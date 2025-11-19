7eddcebd257396e8f401b7651b6a96117f9532aa
gprerana212-oss <gprerana212@gmail.com>
2025-11-19 11:07:36 +0530

    Update student_details.py

--- student_details.py
+++ student_details.py
@@ -1,30 +1,18 @@
 import sys
 
-if len(sys.argv) != 5:
-    print("Usage: python student_details.py <name> <USN> <department> <semester>")
-    sys.exit()
 
+if len(sys.argv) != 5:
+    print("Usage: python student_profile.py <Name> <USN> <Department> <Semester>")
+    sys.exit()
 student_name = sys.argv[1]
 USN = sys.argv[2]
 department = sys.argv[3]
 semester = sys.argv[4]
 
-print("Student Details Program")
 
-
-valid_student_name = "xyz"
-valid_USN = "01FE23BCA021"
-valid_department = "bca"
-valid_semester = "3"
-
-
-if (student_name == valid_student_name and
-    USN == valid_USN and
-    department == valid_department and
-    semester == valid_semester):
-    
-    print("Student details are VALID")
-else:
-    print("Enter valid student details")
-
-
+print("\n========== STUDENT PROFILE CARD ==========")
+print(f"Name       : {student_name}")
+print(f"USN        : {USN}")
+print(f"Department : {department}")
+print(f"Semester   : {semester}")
+print("===========================================\n")
