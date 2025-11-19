4081e7914f20ae2ab0335dea4b880aea8dc23e16
gprerana212-oss <gprerana212@gmail.com>
2025-11-19 11:22:25 +0530

    Update student_details.py

--- student_details.py
+++ student_details.py
@@ -2,18 +2,21 @@ import sys
 
 
 if len(sys.argv) != 5:
-    print("Usage: python student_profile.py <student_Name> <USN> <Department> <Semester>")
+    print("Usage: python student_profile.py <Name> <USN> <Department> <Semester>")
     sys.exit()
-else:
+
+
 student_name = sys.argv[1]
 USN = sys.argv[2]
 department = sys.argv[3]
 semester = sys.argv[4]
 
-
-print("\n========== STUDENT PROFILE CARD ==========")
-print(f"Name       : {student_name}")
-print(f"USN        : {USN}")
-print(f"Department : {department}")
-print(f"Semester   : {semester}")
-print("===========================================\n")
+if student_name and USN and department and semester:
+    print("\n========== STUDENT PROFILE CARD ==========")
+    print(f"Name       : {student_name}")
+    print(f"USN        : {USN}")
+    print(f"Department : {department}")
+    print(f"Semester   : {semester}")
+    print("===========================================\n")
+else:
+    print("Invalid input: All fields are required!")
